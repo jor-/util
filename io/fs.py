69b8c354b48e03b74961f309d597c1ec93df0bda
Joscha Reimer <jor@informatik.uni-kiel.de>
2015-11-26 14:10:15 +0100

    MAINT: util.io.fs: file permission refactored: group and other in changed honored.

--- io/fs.py
+++ io/fs.py
@@ -70,50 +70,66 @@ def get_files(path=os.getcwd(), regular_expression=None):
 
 ## permissions
 
+def file_mode(file):
+    return os.stat(file)[stat.ST_MODE]
+
+def file_permission(file):
+    permission_octal_string = oct(file_mode(file))[-3:]
+    permission_int = int(permission_octal_string, 8)
+    return permission_int
+
+def remove_write_permission(file):
+    permission_old = file_mode(file)
+    permission_new = permission_old
+    for write_permission in (stat.S_IWUSR, stat.S_IWGRP, stat.S_IWOTH):
+        permission_new = permission_new & ~ write_permission
+    os.chmod(file, permission_new)
+    logger.debug('Removing write permission of file {}. Mode changed from {} to {}.'.format(file, oct(permission_old)[-3:],oct( permission_new)[-3:]))
+
+def add_write_permission(file):
+    permission_old = file_mode(file)
+    permission_new = permission_old
+    for read_permission, write_permission in ((stat.S_IRUSR, stat.S_IWUSR), (stat.S_IRGRP, stat.S_IWGRP), (stat.S_IROTH, stat.S_IWOTH)):
+        if permission_new & read_permission:
+            permission_new = permission_new | write_permission
+    os.chmod(file, permission_new)
+    logger.debug('Adding write permission to file {}. Mode changed from {} to {}.'.format(file, oct(permission_old)[-3:], oct(permission_new)[-3:]))
+    
+
 def make_read_only(*files, not_exist_ok=False):
     for file in files:
-        logger.debug('Making {} read-only.'.format(file))
         if not_exist_ok:
             try:
-                os.chmod(file, stat.S_IRUSR)
+                remove_write_permission(file)
             except FileNotFoundError:
                 logger.debug('File {} not existing, but this is okay.'.format(file))
                 pass
         else:
-            os.chmod(file, stat.S_IRUSR)
+            remove_write_permission(file)
 
 def make_read_only_recursively(path, exclude_dir=True):
     logger.debug('Making recursively all files in {} read-only.'.format(path))
-
-    file_function = lambda file: os.chmod(file, stat.S_IRUSR)
-    dir_function = lambda file: os.chmod(file, stat.S_IRUSR | stat.S_IXUSR)
+    file_function = lambda file: make_read_only(file)
+    dir_function = lambda file: make_read_only(file)
     walk_path(path, file_function, dir_function, exclude_dir=exclude_dir, topdown=False)
 
 
 def make_writable(file, not_exist_ok=False):
-    logger.debug('Making {} writable.'.format(file))
     if not_exist_ok:
         try:
-            os.chmod(file, get_file_permission(file) | stat.S_IWUSR)
+            add_write_permission(file)
         except FileNotFoundError:
             logger.debug('File {} not existing, but this is okay.'.format(file))
             pass
     else:
-        os.chmod(file, get_file_permission(file) | stat.S_IWUSR)
+        add_write_permission(file)
 
 
 def make_writable_recursively(path, exclude_dir=True):
     logger.debug('Making recursively all files in {} writeable.'.format(path))
-
-    file_function = lambda file: os.chmod(file, stat.S_IRUSR | stat.S_IWUSR)
-    dir_function = lambda file: os.chmod(file, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+    file_function = lambda file: make_writable(file)
+    dir_function = lambda file: make_writable(file)
     walk_path(path, file_function, dir_function, exclude_dir=exclude_dir, topdown=False)
-
-
-def get_file_permission(file):
-    permission_octal_string = oct(os.stat(file).st_mode)[-3:]
-    permission_int = int(permission_octal_string, 8)
-    return permission_int
 
 
 ## remove
@@ -135,11 +151,16 @@ def remove_recursively(path, force=False, exclude_dir=False):
     walk_path(path, os.remove, os.rmdir, exclude_dir=exclude_dir, topdown=False)
 
 
-# ## extend
-# def extend_filename(file):
-#     file = os.path.expanduser(file)
-#     file = os.path.expandvars(file)
-#     if
+## make
+
+def makedirs(file, exist_ok=True):
+    dir = os.path.dirname(file)
+    if dir is not None:
+        if not os.path.exists(dir):
+            logger.debug('Creating recrusive directories {}.'.format(dir))
+            os.makedirs(dir, exist_ok=exist_ok)
+    else:
+        raise ValueError('Directories of file {} is not creatable.'.format(file))
 
 
 ##
@@ -151,13 +172,6 @@ def flush_and_close(file):
     while not os.path.exists(file.name):
         logger.warning('File {} is not available after flush and fsync. Waiting.'.format(file.name))
         time.sleep(1)
-
-
-def makedirs_if_not_exists(file):
-    dir = os.path.dirname(file)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-        logger.debug('directory {} created'.format(dir))
 
 
 def walk_path(path, file_function, dir_function, topdown=True, exclude_dir=True):
