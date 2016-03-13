590ef23dadb090f44ded949016e80f0e63e0dbc1
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-03-13 19:38:48 +0100

    ENH: util/math/util.py: min_int_dtype method added

--- math/util.py
+++ math/util.py
@@ -3,6 +3,7 @@ import numpy as np
 import util.logging
 logger = util.logging.logger
 
+
 def max_dtype(a, b):
     logger.debug('Calculating max dtype of {} and {}.'.format(a,b))
 
@@ -26,3 +27,27 @@ def max_dtype(a, b):
             return b
 
     raise ValueError('Dtype {} and {} are not comparable.'.format(a,b))
+
+
+
+def min_int_dtype(*values, unsigned=False):    
+    ## get max value
+    if len(values) > 0:
+        max_value = max(values)
+    else:
+        max_value = 0
+
+    ## get dtype
+    if unsigned:
+        int_dtypes = (np.uint8, np.uint16, np.uint32, np.uint64)
+    else:
+        int_dtypes = (np.int8, np.int16, np.int32, np.int64)
+    i = 0
+    while np.iinfo(int_dtypes[i]).max < max_value:
+        i = i+1
+    dtype = int_dtypes[i]
+    
+    ## return 
+    logger.debug('Minimal int dtype (unsigned={}) for values {} is {}.'.format(unsigned, values, dtype))
+    return dtype
+
