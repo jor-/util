ecabdd4ef29ef0b48e33161104eafeb51f14b3a2
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-03-13 19:48:34 +0100

    ENH: util/plot.py: contour plots added; several plots enhanced

--- plot.py
+++ plot.py
@@ -1,23 +1,33 @@
-import numpy as np
 import os.path
 import subprocess
 
+import numpy as np
+import scipy.sparse
+
 import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import matplotlib.colorbar
 import matplotlib.colors
 import matplotlib.font_manager
 import matplotlib.text
 import matplotlib.ticker
-import matplotlib.pyplot as plt
+import mpl_toolkits.axes_grid1
 
 import util.io.fs
 import util.logging
 logger = util.logging.logger
 
+DEFAULT_FONT_SIZE = 20
+
 
 ## plot types
 
-def data(data, file, land_value=np.nan, no_data_value=np.inf, use_log_scale=False, v_min=None, v_max=None, tick_font_size=20, power_limits=(-2,2), dpi=100, colorbar_kwargs={'fraction':0.021, 'pad':0.05, 'aspect':20, 'orientation':'vertical'}):
+def data(data, file, land_value=np.nan, no_data_value=np.inf, land_brightness=0, use_log_scale=False, v_min=None, v_max=None, caption=None, tick_font_size=DEFAULT_FONT_SIZE, power_limit=3, dpi=100, contours=False, contours_text_brightness=0.5, colorbar=True):
+    
+    if data.dtype == np.float128:
+        data = data.astype(np.float64)
+    
 
     def get_masks(data, land_value=np.nan, no_data_value=0):
         def get_value_mask(array, value):
@@ -38,9 +48,6 @@ def data(data, file, land_value=np.nan, no_data_value=np.inf, use_log_scale=Fals
 
     logger.debug('Plotting data.')
 
-#     ## cast to float64
-#     data = data.astype(np.float, copy=False)
-
     ## set font size
     set_global_font_size(tick_font_size)
 
@@ -56,12 +63,16 @@ def data(data, file, land_value=np.nan, no_data_value=np.inf, use_log_scale=Fals
 
     ## get masks and v_min and v_max
     (land_mask, no_data_mask) = get_masks(data, land_value=land_value, no_data_value=no_data_value)
-    if v_min is None or v_max is None:
+    if v_min is None or v_max is None or contours:
         data_mask = np.logical_not(np.logical_or(no_data_mask, land_mask))
-        if v_min is None:
-            v_min = np.min(data[data_mask])
-        if v_max is None:
-            v_max = np.max(data[data_mask])
+        data_min = np.min(data[data_mask])
+        data_max = np.max(data[data_mask])
+
+    ## v_min and v_max
+    if v_min is None:
+        v_min = data_min
+    if v_max is None:
+        v_max = data_max
     logger.debug('Using {} as v_min and {} as v_max.'.format(v_min, v_max))
 
 
@@ -104,19 +115,19 @@ def data(data, file, land_value=np.nan, no_data_value=np.inf, use_log_scale=Fals
 
             current_file += file_extension
 
-            ## make no_data with 1 where no data, 0.5 where water at surface, 0 where land and nan where data
+            ## make no_data with 1 where no data, 0.5 where water at surface, 0 where land and nan where data (1 is white, 0 is black)
             no_data_array = no_data_array * np.nan
             no_data_array[no_data_mask[t,:,:,z]] = 1
-            no_data_array[land_mask[t,:,:,z]] = 0.5
-            no_data_array[land_mask[t,:,:,0]] = 0
-
+            no_data_array[land_mask[t,:,:,z]] = (1 + 9 * land_brightness) / 10
+            no_data_array[land_mask[t,:,:,0]] = land_brightness
+            
 
             ## make figure
             fig = plt.figure()
 
             ## chose colormap
             colormap = plt.cm.jet
-            colormap.set_bad('k',1.)
+            colormap.set_bad(color='w', alpha=0.0)
 
             ## chose norm
             if use_log_scale:
@@ -126,35 +137,71 @@ def data(data, file, land_value=np.nan, no_data_value=np.inf, use_log_scale=Fals
                     norm = matplotlib.colors.BoundaryNorm(np.arange(v_min, v_max+1), colormap.N)
                 else:
                     norm = None
-
-            ## plot data
-            axes_image = plt.imshow(data[t,:,:,z].transpose(), origin='lower', aspect='equal', cmap=colormap, vmin=v_min, vmax=v_max, norm=norm)
-
-            ## plot colorbar
-#             cb = fig.colorbar(axes_image, fraction=.021, pad=.05, aspect=20)
-#             cb = fig.colorbar(axes_image, fraction=.021, pad=.05, aspect=20, orientation=colorbar_orientation)
-            cb = fig.colorbar(axes_image, **colorbar_kwargs)
-#             axc, kw = matplotlib.colorbar.make_axes(plt.gca(), location=colorbar_location)
-#             cb = matplotlib.colorbar.Colorbar(axc, axes_image, **kw)
-#             if colorbar_location in ('left', 'right'):
-#                 cb.ax.yaxis.set_ticks_position(colorbar_location)
-            if not use_log_scale:
-                cb.formatter.set_powerlimits(power_limits)
-                cb.update_ticks()
-
+                
 
             ## plot no data mask
             colormap_no_data = plt.cm.gray
             colormap_no_data.set_bad(color='w', alpha=0.0)
             axes_image = plt.imshow(no_data_array.transpose(), origin='lower', aspect='equal', cmap=colormap_no_data, vmin=0, vmax=1)
 
+            ## plot data
+            current_data = data[t,:,:,z].transpose()
+            axes_image = plt.imshow(current_data, origin='lower', aspect='equal', cmap=colormap, vmin=v_min, vmax=v_max, norm=norm)
 
             ## disable axis labels
             plt.axis('off')
+            
+            ## chose tick locator
+            if colorbar or contours:
+                ## chose tick base
+                if colorbar:
+                    v_max_tick = v_max
+                else:
+                    current_data_max = np.nanmax(current_data)
+                    v_max_tick = min([v_max, current_data_max])
+                    if current_data_max == 0:
+                        contours = False
+                        v_max_tick = 1
+                tick_base_exp = int(np.ceil(np.log10(v_max_tick))) - 1
+                tick_base = 10 ** tick_base_exp
+                
+                if (current_data > tick_base).sum() < (~np.isnan(current_data)).sum() / 100:    # decrease tick if too few data above tick
+                    tick_base = tick_base / 10
+                
+                ## chose locator
+                if use_log_scale:
+                    tick_locator = plt.LogLocator(base=tick_base, subs=tick_base/10)
+                else:
+                    tick_locator = plt.MultipleLocator(base=tick_base)
 
-#             ## set tick font size
-#             set_tick_font(fig.gca(), size=tick_font_size)
-#             set_tick_font(cb.ax.axes, size=tick_font_size)
+            ## plot colorbar
+            if colorbar:
+                divider = mpl_toolkits.axes_grid1.make_axes_locatable(plt.gca())
+                cax = divider.append_axes("right", size="3%", pad=0.1) 
+                               
+                cb = plt.colorbar(axes_image, cax=cax)
+                cb.locator = tick_locator
+                cb.update_ticks()
+                
+                if not use_log_scale:
+                    cb.formatter.set_powerlimits((-power_limit, power_limit))
+                    cb.update_ticks()
+            
+            ## plot contours
+            if contours:
+                contour_plot = plt.contour(current_data, locator=tick_locator, colors='k', linestyles=['dashed', 'solid'], linewidths=0.5, norm=norm)
+                if np.abs(tick_base_exp) >= power_limit:
+                    label_fmt = '%.0e'
+                elif tick_base_exp < 0:
+                    label_fmt = '%1.{:d}f'.format(np.abs(tick_base_exp))
+                else:
+                    label_fmt = '%d'
+                plt.clabel(contour_plot, contour_plot.levels[1::2], fontsize=8, fmt=label_fmt, colors=str(contours_text_brightness))
+            
+    
+            ## set caption
+            if caption is not None:
+                plt.xlabel(caption, fontsize=font_size, fontweight='bold')
 
             ## save and close
             save_and_close_fig(fig, current_file, dpi=dpi)
@@ -165,16 +212,38 @@ def data(data, file, land_value=np.nan, no_data_value=np.inf, use_log_scale=Fals
 
 
 
-def line(x, y, file, line_label=None, line_width=1, line_style='-', line_color='r', y_min=None, y_max=None, xticks=None, spine_line_width=1, use_log_scale=False, transparent=True, tick_font_size=20, legend_font_size=16, x_label=None, y_label=None, axis_label_font_size=16, dpi=800):
+def line(x, y, file, x_order=0, line_label=None, line_width=1, line_style='-', line_color='r', y_min=None, y_max=None, xticks=None, spine_line_width=1, use_log_scale=False, transparent=True, tick_font_size=DEFAULT_FONT_SIZE, legend_font_size=DEFAULT_FONT_SIZE, x_label=None, y_label=None, axis_label_font_size=DEFAULT_FONT_SIZE, dpi=800):
     logger.debug('Plotting line.')
-
-    if len(x) != len(y) and len(x) != 1:
-        raise ValueError('Length of x must have same length as y or length 1 but length x is {} and length y is {}.'.format(len(x), len(y)))
+    
+    ## check if multi line
+    try:
+        y[0][0]
+    except LookupError:
+        is_multi_line = False
+    else:
+        is_multi_line = True
+    
+    ## check input and prepare
+    if len(x) != len(y):
+        if not is_multi_line:
+                ValueError('For single line plot, x and y must have same length but length x is {} and length y is {}.'.format(len(x), len(y)))
+        else:
+            x = np.asarray(x)
+            y = np.asarray(y)
+            assert y.ndim == 2
+            if x.ndim != 1:
+                raise ValueError('For multiline plot, if x and y have two dims, the x and y must have same length but length x is {} and length y is {}.'.format(len(x), len(y)))
+            else:
+                if x.shape[0] != y.shape[1]:
+                    raise ValueError('For multiline plot, if x has one dim and y has two dims, the first dim of x must have the same size as the second dim of y, but shape x is {} and shape y is {}.'.format(x.shape, y.shape))
+                else:
+                    x = np.tile(x, y.shape[1]).reshape(y.shape[1], -1)
+        
 
     ## prepare line(s)
     if len(y) > 0:
         ## multiple lines
-        if np.asarray(y[0]).ndim != 0:
+        if is_multi_line:
             xs = x
             ys = y
     
@@ -183,7 +252,7 @@ def line(x, y, file, line_label=None, line_width=1, line_style='-', line_color='
             line_setups = []
             for line_setup in (line_label, line_width, line_style, line_color):
                 if np.asarray(line_setup).ndim == 0:
-                    line_setup = (line_setup,) * n
+                    line_setup = (line_setup,) * len(y)
                 line_setups.append(line_setup)
     
             line_labels = line_setups[0]
@@ -222,9 +291,12 @@ def line(x, y, file, line_label=None, line_width=1, line_style='-', line_color='
             line_color = line_colors[i]
 
             ## sort values
-            sorted_indices = np.argsort(x)
-            x = x[sorted_indices]
-            y = y[sorted_indices]
+            if x_order != 0:
+                sorted_indices = np.argsort(x)
+                if x_order < 0:
+                    sorted_indices = sorted_indices[::-1]
+                x = x[sorted_indices]
+                y = y[sorted_indices]
 
             ## update x_min x_max
             x_min = min([x_min, x[0]])
@@ -233,12 +305,6 @@ def line(x, y, file, line_label=None, line_width=1, line_style='-', line_color='
             ## plot line
             plt.plot(x, y, line_style, color=line_color, linewidth=line_width, markersize=line_width*3, label=line_label)
 
-    ## set axis limits
-    if y_min is not None:
-        plt.ylim(ymin=y_min)
-    if y_max is not None:
-        plt.ylim(ymax=y_max)
-    plt.xlim(x_min, x_max)
 
     ## set axis labels
     if x_label is not None:
@@ -257,6 +323,13 @@ def line(x, y, file, line_label=None, line_width=1, line_style='-', line_color='
     ## log scale
     if use_log_scale:
         plt.yscale('log')
+
+    ## set axis limits
+    if y_min is not None:
+        plt.ylim(ymin=y_min)
+    if y_max is not None:
+        plt.ylim(ymax=y_max)
+    plt.xlim(x_min, x_max)
 
     ## legend
     legend = plt.legend(loc=0)
@@ -291,7 +364,7 @@ def scatter(x, y, file, point_size=20, dpi=800):
 
 
 
-def histogram(data, file, bins=None, step_size=None, x_min=None, x_max=None, weights=None, use_log_scale=False, type='bar', tick_font_size=20, tick_power=None, dpi=800):
+def histogram(data, file, bins=None, step_size=None, x_min=None, x_max=None, weights=None, use_log_scale=False, type='bar', tick_font_size=DEFAULT_FONT_SIZE, tick_power=None, dpi=800):
     logger.debug('Plotting histogram.')
 
     ## make fig
@@ -312,32 +385,94 @@ def histogram(data, file, bins=None, step_size=None, x_min=None, x_max=None, wei
     plt.xlim(bins[0], bins[-1])
 
     ## set axis label size
-#     set_tick_power_limits(axis='y', power_limits=(-2,2))
     if tick_power is not None:
         set_tick_power_fix(fig.gca(), axis='y', power=tick_power)
     set_tick_font(fig.gca(), size=tick_font_size)
 
-
     ## save and close
     save_and_close_fig(fig, file, dpi=dpi)
 
 
 
+def spy(A, file, markersize=1, axis_labels=True, caption=None, font_size=DEFAULT_FONT_SIZE, dpi=800):
 
-
-def spy(A, file, markersize=1, dpi=800):
-    logger.debug('Plotting sparsity pattern for matrix {!r} with markersize {} and dpi {} to file {}.'.format(A, markersize, dpi, file))
+    ## set font size
+    set_global_font_size(tick_font_size)
 
     ## make figure
     fig = plt.figure()
+    
+    ## plot sparsity_pattern
+    if scipy.sparse.issparse(A):
+        logger.debug('Plotting sparsity pattern for matrix {!r} with markersize {} and dpi {} to file {}.'.format(A, markersize, dpi, file))
+        plt.spy(A, markersize=markersize, marker=',', markeredgecolor='k', markerfacecolor='k')
+    
+    ## plot matrix values
+    else:
+        logger.debug('Plotting values for matrix {!r} with dpi {} to file {}.'.format(A, dpi, file))
+        v_abs_max = np.abs(A).max()
+        axes_image = plt.imshow(A, cmap=plt.cm.bwr, interpolation='nearest', vmin=-v_abs_max, vmax=v_abs_max)   
+        cb = fig.colorbar(axes_image)
+        cb.ax.tick_params(labelsize=font_size) 
+        fig.gca().set_xticks([]) 
+        fig.gca().set_yticks([]) 
 
-    ## plot
-    plt.spy(A, markersize=markersize, marker=',', markeredgecolor='k', markerfacecolor='k')
-
+    ## set power limits
+    if axis_labels:
+        formatter = plt.ScalarFormatter()
+        formatter.set_powerlimits((-3,3))
+        fig.gca().xaxis.set_major_formatter(formatter)
+        fig.gca().yaxis.set_major_formatter(formatter)
+    
+    ## disable axis labels
+    else:
+        plt.axis('off')
+    
+    ## set caption
+    if caption is not None:
+        plt.xlabel(caption, fontsize=font_size, fontweight='bold')
+    
     ## save and close
     save_and_close_fig(fig, file, dpi=dpi)
 
 
+
+
+def intervals(intervals, file, use_percent_ticks=False, caption=None, font_size=DEFAULT_FONT_SIZE, dpi=800):
+    intervals = np.asanyarray(intervals)
+    assert intervals.ndim == 2
+    assert len(intervals) == 2
+    
+    ## calculate data
+    intervals_half_sizes = (intervals[1] - intervals[0]) / 2
+    means = intervals_half_sizes + intervals[0]
+    n = len(intervals_half_sizes)
+
+    ## set font size
+    set_global_font_size(font_size)
+
+    ## make figure
+    fig = plt.figure()
+    
+    ## plot
+    linewidth = 3
+    plt.errorbar(np.arange(1, n+1), means, xerr=0, yerr=intervals_half_sizes, linestyle='', linewidth=linewidth, elinewidth=linewidth, capsize=linewidth*3, capthick=linewidth, marker='.', markersize=linewidth*5)
+    
+    ## set y limits
+    plt.xlim(0.5, n + 0.5)
+    
+    ## y tick formatter
+    if use_percent_ticks:
+        fmt = '%.0f%%'
+        yticks = matplotlib.ticker.FormatStrFormatter(fmt)
+        fig.gca().yaxis.set_major_formatter(yticks)
+
+    ## set caption
+    if caption is not None:
+        plt.xlabel(caption, fontsize=font_size, fontweight='bold')
+    
+    ## save and close
+    save_and_close_fig(fig, file, dpi=dpi)
 
 
 
@@ -375,12 +510,7 @@ def set_font_all(fig, family='sans-serif', weight='bold', size=12):
 
 
 def set_tick_font(axes, family='sans-serif', weight='bold', size=12):
-#     font = {'family':family, 'weight':weight, 'size':size}
-#     axes = fig.gca()
-#     for label in (axes.get_xticklabels() + axes.get_yticklabels()):
-#         label.set_fontproperties(font)
     font_properties = matplotlib.font_manager.FontProperties(family=family, weight=weight, size=size)
-#     axes = fig.gca()
     plt.setp(axes.get_xticklabels(), fontproperties=font_properties)
     plt.setp(axes.get_xaxis().get_offset_text(), fontproperties=font_properties)
     plt.setp(axes.get_yticklabels(), fontproperties=font_properties)
@@ -415,10 +545,6 @@ def set_legend_font(fig, family='sans-serif', weight='bold', size=12):
     axes = fig.gca()
     legend = axes.get_legend()
     plt.setp(legend.get_texts(), fontproperties=font_properties)
-#     for text in legend.get_texts():
-#         text.set_fontproperties(font)
-#     plt.setp(legend.get_texts(), fontsize=legend_font_size)
-#     plt.setp(legend.get_texts(), fontproperties=font)
 
 
 def set_global_font_size(size=20):
@@ -435,6 +561,4 @@ def get_colors(n, colormap_name='gist_rainbow'):
     colormap = plt.get_cmap(colormap_name)
     colors = [colormap(i/(n-1)) for i in range(n)]
     return colors
-
-
 
