59de9bafa8909e2943fc905b089499e531e0e90a
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-03-13 19:39:41 +0100

    ENH: util/math/sparse/create.py: __setitem__ added and coo_matrix renamed to tocoo

--- math/sparse/create.py
+++ math/sparse/create.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.sparse
 import scipy.sparse.sputils
 
+import util.math.util
 import util.logging
 logger = util.logging.logger
 
@@ -14,7 +15,6 @@ def csr_matrix(value_function, shape, value_range, dtype=None, number_of_process
 
     ## prepare values for pool
     if number_of_processes is None:
-        # number_of_processes = max([multiprocessing.cpu_count() - 1, 1])
         number_of_processes = multiprocessing.cpu_count()
     if chunksize is None:
         chunksize = max([int(len(value_range) / (number_of_processes*10**3)), 1])
@@ -58,9 +58,6 @@ def diag(d):
     return D
 
 
-def min_int_dtype(shape):
-    return scipy.sparse.sputils.get_index_dtype(maxval=max(shape))
-
 
 def list_to_array(values, dtype=None):
     # a = np.asarray(values, dtype=dtype) #-> numpy bug
@@ -77,12 +74,12 @@ class InsertableMatrix():
 
     def __init__(self, shape, dtype=np.float64):
         self.shape = shape
-        self.indices_dtype = min_int_dtype(shape)
         self.data_dtype = dtype
         self.row_indices = []
         self.colum_indices = []
         self.data = []
-        logger.debug('Initiating insertable matrix with shape {}, index dtype {} and data dtype {}.'.format(self.shape, self.indices_dtype, self.data_dtype))
+        self.nnz = 0
+        logger.debug('Initiating insertable matrix with shape {} and data dtype {}.'.format(self.shape, self.data_dtype))
 
 
     def insert(self, i, j, v):
@@ -90,14 +87,22 @@ class InsertableMatrix():
         self.colum_indices.append(j)
         self.data.append(v)
         assert len(self.row_indices) == len(self.colum_indices) == len(self.data)
+        assert len(self.data) == self.nnz + 1
+        self.nnz = self.nnz + 1
+    
+    
+    def __setitem__(self, index, value):
+        i, j = index
+        self.insert(i, j, value)
 
-    def coo_matrix(self):
-        logger.debug('Prepare coo matrix with {} entries, index dtype {} and data dtype {}.'.format(len(self.data), self.indices_dtype, self.data_dtype))
-        # row = np.asarray(self.row_indices, dtype=self.indices_dtype)
-        # col = np.asarray(self.colum_indices, dtype=self.indices_dtype)
-        # data = np.asarray(self.data, dtype=self.data_dtype)
-        row = list_to_array(self.row_indices, dtype=self.indices_dtype)
-        col = list_to_array(self.colum_indices, dtype=self.indices_dtype)
+
+    def tocoo(self):
+        logger.debug('Prepare coo matrix with {} entries and data dtype {}.'.format(len(self.data), self.data_dtype))
+        assert len(self.row_indices) == len(self.colum_indices) == len(self.data)
+
+        indices_dtype = util.math.util.min_int_dtype(*self.shape)
+        row = list_to_array(self.row_indices, dtype=indices_dtype)
+        col = list_to_array(self.colum_indices, dtype=indices_dtype)
         data = list_to_array(self.data, dtype=self.data_dtype)
 
         matrix = scipy.sparse.coo_matrix((data, (row, col)), shape=self.shape)
@@ -107,6 +112,7 @@ class InsertableMatrix():
         logger.debug('Returning matrix {!r}.'.format(matrix))
         assert matrix.dtype == self.data_dtype
         return matrix
+
 
     def asformat(self, format='csc'):
         logger.debug('Prepare {} matrix with {} entries and dtype {}.'.format(format, len(self.data), self.data_dtype))
