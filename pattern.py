34ed941a350bc10cd05f81872b6bec784c5dac7d
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-05-25 10:12:12 +0200

    ENH: pattern: convert_format_string_in_regular_expression method add

--- pattern.py
+++ pattern.py
@@ -1,14 +1,14 @@
 import re
 
 
-def is_matching(string, pattern, integer_pattern='{:0>\d+}'):
-    for integer_pattern_i in re.findall(integer_pattern, pattern):
-            pattern = pattern.replace(integer_pattern_i, '\d+')
-    find_all = re.findall(pattern, string)
-    is_matching = len(find_all) == 1 and string == find_all[0]
-
-    return is_matching
-
-
 def get_int_in_string(string):
     return int(re.search(r'\d+', string).group())
+
+
+def convert_format_string_in_regular_expression(format_string):
+    regular_expression = format_string
+    for pattern in re.findall('{.*:.*d}', regular_expression):
+        regular_expression = regular_expression.replace(pattern, '[+,-]?[0-9]*')
+    for pattern in re.findall('{.*}', regular_expression):
+        regular_expression = regular_expression.replace(pattern, '.*')
+    return regular_expression
