e7f6469b98df47eb50222f7c05819e4bff969cbc
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-08-18 12:09:35 +0200

    BLD: setup.py and README.rst added

---
+++ README.rst
@@ -0,0 +1,25 @@
+====
+UTIL
+====
+
+What is this?
+--------------
+
+This is a collection of Python modules containing auxiliary functions used in serveral other projects.
+
+
+Web sites
+---------
+
+The user's site is:
+
+    https://github.com/jor-/util
+
+
+Author
+------
+
+Joscha Reimer
+
+    jor@informatik.uni-kiel.de
+
---
+++ setup.py
@@ -0,0 +1,82 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+"""
+
+import setuptools
+import os.path
+
+# Get the long description from the README file
+readme_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+with open(readme_file, mode='r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='util',
+
+    # Version
+    # Should comply with PEP440.
+    # https://www.python.org/dev/peps/pep-0440/
+    #version='0.1.1',
+    use_scm_version=True,
+    setup_requires=['setuptools>=0.8', 'pip>=1.4','setuptools_scm'],
+
+    description='util functions',
+    long_description=long_description,
+
+    # Homepage
+    url='https://github.com/jor-/util',
+
+    # Author
+    author='Joscha Reimer',
+    author_email='jor@informatik.uni-kiel.de',
+
+    # License
+    # license='MIT',
+
+    # Classifiers
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # Development Status
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Intended Audience, Topic
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        # Licence (should match "license" above)
+        #'License :: OSI Approved :: MIT License',
+
+        # Supported Python versions
+        'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.5',scikits
+    ],
+
+    # Keywords
+    keywords='utility auxiliary functions',
+
+    # Packages to install
+    packages=setuptools.find_packages(),
+
+    # Dependencies
+    install_requires=['numpy'],
+    extras_require={
+        'cache': ['cachetools'],
+        'sorted_multi_dict': ['blist'],
+        'colored_log': ['colorlog'],
+        'hdf5': ['h5py'],
+        'options': ['h5py'],
+        'netcdf': ['netCDF4', 'scipy'],
+        'petsc': ['petsc4py'],
+        'cholmod': ['scikit-sparse'],
+        'scoop': ['scoop'],
+        'deap': ['deap'],
+        'plot' : ['matplotlib', 'scipy'],
+        'sparse' : ['scipy'],
+        'multi_dict_stats' : ['scipy']
+    }
+)
