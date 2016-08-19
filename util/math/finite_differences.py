bff51f03d8fa45b602f44b69dd3ee21287e7a2f0
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-08-19 13:07:32 +0200

    API: util.math.finite_differences: changed partial derivatives to first dim; convert inputs to arrays where needed and ensure right dtypes; calculate f(x) only when needed

--- util/math/finite_differences.py
+++ util/math/finite_differences.py
@@ -2,16 +2,18 @@ import numpy as np
 
 
 
-def calculate(f, x, f_x=None, typical_x=None, bounds=None, accuracy_order=2, eps=np.spacing(1)):
+def calculate(f, x, f_x=None, typical_x=None, bounds=None, accuracy_order=2, eps=np.spacing(1), use_always_typical_x=True):
+    x = np.asanyarray(x)
+    
     ## init unpassed values
-    if f_x is None:
-        f_x =  f(x)
     if typical_x is None:
         typical_x = np.ones_like(x)
     elif not len(x) == len(typical_x):
         raise ValueError('x and typical_x must have the same length but their length are {} and {}.'.format(len(x), len(typical_x)))
     if bounds is None:
         bounds = ((-np.inf, np.inf),) * len(x)
+    else:
+        bounds = np.asanyarray(bounds)
 
     ## set h factors according to accuracy
     if accuracy_order == 1:
@@ -22,27 +24,35 @@ def calculate(f, x, f_x=None, typical_x=None, bounds=None, accuracy_order=2, eps
         eta = eps**(1/3)
     else:
         raise ValueError('Accuracy order {} not supported.'.format(accuracy_order))
+    
+    ## calculate f(x) if needed
+    if f_x is None and accuracy_order == 1:
+        f_x =  f(x)
 
     ## init values
-    f_x = np.asarray(f_x)
     try:
         l = len(f_x)
     except TypeError:
         l = 1
     n = len(x)
     m = len(h_factors)
-    df = np.zeros([l, n])
+    df = None
 
     ## for each x dim
     for i in range(n):
-        h = np.empty(m)
+        h = np.empty(m, dtype=np.float64)
+        
         ## for each h factor
         for j in range(m):
             ## calculate h
-            h[j] = eta * np.max([np.abs(x[i]), np.abs(typical_x[i])]) * h_factors[j]
-            if x[i] < 0:
+            if use_always_typical_x:
+                typical_x_i = np.abs(typical_x[i])
+            else:
+                typical_x_i = np.max([np.abs(typical_x[i]), np.abs(x[i])])
+            h[j] = h_factors[j] * eta * typical_x_i
+            if accuracy_order == 1 and x[i] < 0:
                 h[j] = - h[j]
-            x_h = np.copy(x)
+            x_h = np.array(x, dtype=np.float64, copy=True)
             x_h[i] += h[j]
 
             ## consider bounds
@@ -61,18 +71,25 @@ def calculate(f, x, f_x=None, typical_x=None, bounds=None, accuracy_order=2, eps
                         x_h[i] = lower_bound
                     else:
                         x_h[i] = upper_bound
+            
+            ## recalculate h   (improvement of accuracy of h)
+            h[j] = x_h[i] - x[i]
 
             ## eval f and add to df
-            h[j] = x_h[i] - x[i]
             f_x_h = f(x_h)
+            f_x_h = np.asanyarray(f_x_h)
 
-            df[:, i] += (-1)**j * f_x_h
+            if df is None:
+                df_shape = (n,) + f_x_h.shape
+                df = np.zeros(df_shape)
+            
+            df[i] += (-1)**j * f_x_h
 
         ## calculate df_i
         if accuracy_order == 1:
-            df[:, i] -= f_x
-            df[:, i] /= h
+            df[i] -= f_x
+            df[i] /= h
         else:
-            df[:, i] /= np.sum(np.abs(h))
+            df[i] /= np.sum(np.abs(h))
 
     return df
