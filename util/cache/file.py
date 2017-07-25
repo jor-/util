f5efcf2471140678a7d89ec0cd6189fac943e99d
Joscha Reimer <jor@informatik.uni-kiel.de>
2017-07-25 14:56:01 +0200

    ENH: util.cache.file: load_function and save_function parameters added

--- util/cache/file.py
+++ util/cache/file.py
@@ -9,10 +9,14 @@ logger = util.logging.logger
 
 ## file cache
 
-def decorator(cache_file_function=None):
-    
-    def decorate(function, cache_file_function=None):
-        ## if no cache file function is passed used passed cache file function name or default cache file function name 
+def decorator(cache_file_function=None, load_function=None, save_function=None):
+    if load_function is None:
+        load_function = util.io.universal.load
+    if save_function is None:
+        save_function = util.io.universal.save
+
+    def decorate(function, cache_file_function=None, load_function=None, save_function=None):
+        ## if no cache file function is passed used passed cache file function name or default cache file function name
         cache_file_function_defined = not (cache_file_function is None or isinstance(cache_file_function, str))
         if not cache_file_function_defined:
             ## passed cache file function name
@@ -22,8 +26,8 @@ def decorator(cache_file_function=None):
             else:
                 function_name = function.__name__
                 cache_file_function_name = '{function_name}_cache_file'.format(function_name=function_name)
-        
-        def wrapper(*args, **kargs):            
+
+        def wrapper(*args, **kargs):
             ## calculate cache file
             if cache_file_function_defined:
                 cache_file = cache_file_function(*args, **kargs)
@@ -40,28 +44,32 @@ def decorator(cache_file_function=None):
                         logger.warn('Cache file {} is not defined in {}. Using no cache!'.format(cache_file_function_name, self))
                     else:
                         cache_file = cache_file_function_by_name(*args[1:], **kargs)
-            
+
             ## if cache file is defined, use cache
-            if cache_file is not None:                
+            if cache_file is not None and load_function is not None:
                 ## if cache file is available, load cached value
                 try:
-                    value = util.io.universal.load(cache_file)
+                    value = load_function(cache_file)
                     logger.debug('Calculated value loaded from cache file {}.'.format(cache_file))
                 ## if cache file is not available, calculate value and save as cached value
                 except OSError:
                     logger.debug('No cached value found for {}.'.format(cache_file))
                     value = function(*args, **kargs)
-                    logger.debug('Saving calculated value to cache file {}.'.format(cache_file))
-                    util.io.universal.save(cache_file, value)
-                    util.io.fs.make_read_only(cache_file)
+                    if save_function is not None:
+                        logger.debug('Saving calculated value to cache file {}.'.format(cache_file))
+                        save_function(cache_file, value)
+                        util.io.fs.apply_recursively(cache_file, util.io.fs.remove_write_permission)
+                    else:
+                        logger.debug('Not saving calculated value to cache file {} because no save function is provided.'.format(cache_file))
+
             ## if cache file not defined, calculate value without cache
             else:
                 value = function(*args, **kargs)
-            
+
             return value
 
         wrapper = util.cache.auxiliary.set_wrapper_attributes(wrapper, function)
         return wrapper
-    
-    return lambda function: decorate(function, cache_file_function=cache_file_function)
+
+    return lambda function: decorate(function, cache_file_function=cache_file_function, load_function=load_function, save_function=save_function)
 
