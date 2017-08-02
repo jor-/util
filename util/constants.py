7e5f3d626cd767d6f123f65db1a4f00c71ac71b2
Joscha Reimer <jor@informatik.uni-kiel.de>
2017-08-02 16:28:55 +0200

    MAINT: util.constants.WORK_DIR: use HOME_DIR if WORK env variable is not set

--- util/constants.py
+++ util/constants.py
@@ -2,8 +2,11 @@ import os.path
 
 import util.io.env
 
-WORK_DIR = util.io.env.load('WORK')
 HOME_DIR = util.io.env.load('HOME')
+try:
+    WORK_DIR = util.io.env.load('WORK')
+except util.io.env.EnvironmentLookupError:
+    WORK_DIR = HOME_DIR
 try:
     TMP_DIR = util.io.env.load('TMP')
 except util.io.env.EnvironmentLookupError:
