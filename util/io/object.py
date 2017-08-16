dc65c317366bd3f7bb8db7c9f1eccd3359c2949f
Joscha Reimer <jor@informatik.uni-kiel.de>
2017-08-16 12:07:39 +0200

    MAINT: util.io.object: logging added

--- util/io/object.py
+++ util/io/object.py
@@ -2,33 +2,37 @@ import pickle
 import pickletools
 
 import util.io.fs
-
+import util.logging
 
 
 FILE_EXT = '.ppy'
+MAX_PROTOCOL = -1
+
 
 def is_file(file):
     return util.io.fs.has_file_ext(file, FILE_EXT)
+
 
 def add_file_ext(file):
     return util.io.fs.add_file_ext_if_needed(file, FILE_EXT)
 
 
-
-def save(file, o, protocol=-1):
+def save(file, obj, protocol=MAX_PROTOCOL):
     file = add_file_ext(file)
+    util.logging.debug('Saving {} to {}.'.format(obj, file))
     with open(file, 'wb') as file_object:
-        pickle.dump(o, file_object, protocol=protocol)
+        pickle.dump(obj, file_object, protocol=protocol)
+
 
 def load(file):
+    util.logging.debug('Loading from {}.'.format(file))
     with open(file, 'rb') as file_object:
-        o = pickle.load(file_object)
-    return o
+        obj = pickle.load(file_object)
+    return obj
 
 
 def protocol_version(file):
-    maxproto = -1
     with open(file, 'rb') as file_object:
         for opcode, arg, pos in pickletools.genops(file_object):
-            maxproto = max(maxproto, opcode.proto)
+            maxproto = max(MAX_PROTOCOL, opcode.proto)
     return maxproto
