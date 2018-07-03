d21366ac8f9cf58d50582e9e477f73beccbb2bde
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-07-03 14:32:16 +0200

    MAINT: setup.py: pep8 errors removed

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -29,17 +29,17 @@ with open(readme_file, mode='r', encoding='utf-8') as f:
 # Setup
 setuptools.setup(
     # general informations
-    name = 'utillib',
-    description = 'util functions',
-    long_description = long_description,
-    keywords = 'utility auxiliary functions',
+    name='utillib',
+    description='util functions',
+    long_description=long_description,
+    keywords='utility auxiliary functions',
 
-    url = 'https://github.com/jor-/util',
-    author = 'Joscha Reimer',
-    author_email = 'jor@informatik.uni-kiel.de',
-    license = 'AGPLv3+',
+    url='https://github.com/jor-/util',
+    author='Joscha Reimer',
+    author_email='jor@informatik.uni-kiel.de',
+    license='AGPLv3+',
 
-    classifiers = [
+    classifiers=[
         # Development Status
         'Development Status :: 3 - Alpha',
         # Intended Audience, Topic
@@ -57,31 +57,31 @@ setuptools.setup(
     cmdclass=versioneer.get_cmdclass(),
 
     # packages to install
-    packages = setuptools.find_packages(),
+    packages=setuptools.find_packages(),
 
     # dependencies
-    setup_requires = [
+    setup_requires=[
         'setuptools>=0.8',
         'pip>=1.4',
     ],
-    install_requires = [
+    install_requires=[
         'numpy',
     ],
-    extras_require = {
+    extras_require={
         'cache': ['cachetools'],
         'hdf5': ['h5py'],
         'options': ['h5py'],
         'sorted_multi_dict': ['blist'],
         'colored_log': ['colorlog'],
         'petsc': ['petsc4py'],
-        'multi_dict_stats' : ['scipy'],
-        'matlab' : ['scipy'],
-        'market' : ['scipy'],
-        'optimize' : ['scipy'],
-        'interpolate' : ['scipy>=0.17'],
-        'sparse' : ['scipy>=0.19'],
+        'multi_dict_stats': ['scipy'],
+        'matlab': ['scipy'],
+        'market': ['scipy'],
+        'optimize': ['scipy'],
+        'interpolate': ['scipy>=0.17'],
+        'sparse': ['scipy>=0.19'],
         'netcdf': ['scipy'],
-        'plot' : ['matplotlib'],
+        'plot': ['matplotlib'],
         'scoop': ['scoop'],
         'deap': ['deap'],
     },
