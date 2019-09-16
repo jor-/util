ed5d56441e2dc338218cde76868dd70e19a36e1e
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-09-16 21:03:28 +0200

    ENH: util.math.finite_differences: dtype for calculation increased to float128

--- util/math/finite_differences.py
+++ util/math/finite_differences.py
@@ -57,17 +57,18 @@ def _step_sizes(x, eps, typical_x=None, use_always_typical_x=True, bounds=None, 
             h[i] = x_h[i] - x[i]
 
     # return
+    assert not both_directions or np.all(h[:, 0] >= h[:, 1])
     return h
 
 
-def _f_x_h(f, x, h, *h_indices):
+def _f_x_h(f, x, h, *h_indices, dtype=np.float):
     # calculate x
     x_h = x.copy()
     for h_index in h_indices:
         x_h[h_index[0]] += h[h_index]
     # eval f
     f_x_h = f(x_h)
-    f_x_h = np.asanyarray(f_x_h)
+    f_x_h = np.asanyarray(f_x_h, dtype=dtype)
     # return
     return f_x_h
 
@@ -79,40 +80,44 @@ def first_derivative(f, x, f_x=None, typical_x=None, bounds=None, eps=None, use_
     x = np.asanyarray(x)
 
     # calculate step size
-    dtype = np.float64
     if eps is None:
         if accuracy_order == 1:
-            eps = np.spacing(1)**(1 / 2)
+            eps = np.finfo(np.float64).eps**(1 / 2)
         elif accuracy_order == 2:
-            eps = np.spacing(1)**(1 / 3)
+            eps = np.finfo(np.float64).eps**(1 / 3)
+    dtype = np.float128
     h = _step_sizes(x, eps, typical_x=typical_x, use_always_typical_x=use_always_typical_x, bounds=bounds, both_directions=accuracy_order >= 2, dtype=dtype)
 
     # define function for evaluation
     def f_x_h(*h_indices):
-        return _f_x_h(f, x, h, *h_indices)
+        return _f_x_h(f, x, h, *h_indices, dtype=dtype)
 
     # init values
     n = len(x)
-    df = None
 
     # calculate f(x)
     if f_x is None and accuracy_order == 1:
         f_x = f(x)
     if f_x is not None:
-        f_x = np.asanyarray(f_x)
+        f_x = np.asanyarray(f_x, dtype=dtype)
+        df = np.zeros((n,) + f_x.shape, dtype=dtype)
+    else:
+        df = None
 
     # calculate df
     for i in range(n):
         if accuracy_order == 1:
-            df_i = f_x_h((i,)) - f_x
-            df_i /= np.sign(h[i]) * h_i
+            df[i] = f_x_h((i,)) - f_x
+            df[i] /= np.sign(h[i]) * h[i]
         elif accuracy_order == 2:
-            df_i = f_x_h((i, 0)) - f_x_h((i, 1))
-            df_i /= np.sum(np.abs(h[i]))
+            f_x_h_0 = f_x_h((i, 0))
+            f_x_h_1 = f_x_h((i, 1))
+            if df is None:
+                df = np.zeros((n,) + f_x_h_0.shape, dtype=dtype)
+            df[i] = f_x_h_0 - f_x_h_1
+            df[i] /= np.sum(np.abs(h[i]), dtype=dtype)
         else:
             assert False
-        if df is None:
-            df = np.zeros((n,) + df_i.shape, dtype=dtype)
         df[i] = df[i]
 
     return df
@@ -125,23 +130,23 @@ def second_derivative(f, x, f_x=None, typical_x=None, bounds=None, eps=None, use
     x = np.asanyarray(x)
 
     # calculate step size
-    dtype = np.float64
     if eps is None:
         if accuracy_order == 1:
-            eps = np.spacing(1)**(1 / 3)
+            eps = np.finfo(np.float64).eps**(1 / 3)
         elif accuracy_order == 2:
-            eps = np.spacing(1)**(1 / 4)
+            eps = np.finfo(np.float64).eps**(1 / 4)
         elif accuracy_order == 4:
-            eps = np.spacing(1)**(1 / 6)
+            eps = np.finfo(np.float64).eps**(1 / 6)
         else:
             assert False
+    dtype = np.float128
     h = _step_sizes(x, eps, typical_x=typical_x, use_always_typical_x=use_always_typical_x, bounds=bounds, both_directions=accuracy_order >= 2, dtype=dtype)
     if accuracy_order > 2:
         h2 = _step_sizes(x, 2 * eps, typical_x=typical_x, use_always_typical_x=use_always_typical_x, bounds=bounds, both_directions=True, dtype=dtype)
 
     # define function for evaluation
     def f_x_h(h, *h_indices):
-        return _f_x_h(f, x, h, *h_indices)
+        return _f_x_h(f, x, h, *h_indices, dtype=dtype)
 
     # init values
     n = len(x)
@@ -149,7 +154,7 @@ def second_derivative(f, x, f_x=None, typical_x=None, bounds=None, eps=None, use
     # calculate f(x)
     if f_x is None:
         f_x = f(x)
-    f_x = np.asanyarray(f_x)
+    f_x = np.asanyarray(f_x, dtype=dtype)
 
     # create df array
     df_shape = (n, n) + f_x.shape
@@ -157,7 +162,7 @@ def second_derivative(f, x, f_x=None, typical_x=None, bounds=None, eps=None, use
 
     # calculate f for changes in a single component
     f_single_h_shape = h.shape + f_x.shape
-    f_single_h = np.empty(f_single_h_shape)
+    f_single_h = np.empty(f_single_h_shape, dtype=dtype)
     for i, h_i in np.ndenumerate(h):
         f_single_h[i] = f_x_h(h, i)
 
@@ -166,21 +171,21 @@ def second_derivative(f, x, f_x=None, typical_x=None, bounds=None, eps=None, use
         for i in range(n):
             for j in range(i + 1):
                 df[i, j] = (f_x_h(h, (i,), (j,)) + f_x) - (f_single_h[i] + f_single_h[j])
-                df[i, j] /= np.abs(h[i]).mean() * np.abs(h[j]).mean()
+                df[i, j] /= np.abs(h[i]).mean(dtype=dtype) * np.abs(h[j]).mean(dtype=dtype)
     elif accuracy_order == 2:
         for i in range(n):
             # diagonal values
             df[i, i] = (f_single_h[i, 0] + f_single_h[i, 1]) - 2 * f_x
-            df[i, i] /= np.abs(h[i]).mean()**2
+            df[i, i] /= np.abs(h[i]).mean(dtype=dtype)**2
             # off diagonal values
             for j in range(i):
                 df[i, j] = (2 * f_x + f_x_h(h, (i, 0), (j, 0)) + f_x_h(h, (i, 1), (j, 1))) - (f_single_h[i, 0] + f_single_h[i, 1] + f_single_h[j, 0] + f_single_h[j, 1])
-                df[i, j] /= 2 * np.abs(h[i]).mean() * np.abs(h[j]).mean()
+                df[i, j] /= 2 * np.abs(h[i]).mean(dtype=dtype) * np.abs(h[j]).mean(dtype=dtype)
     elif accuracy_order == 4:
         for i in range(n):
             # diagonal values
             df[i, i] = (16 * (f_x_h(h, (i, 0)) + f_x_h(h, (i, 1)))) - (30 * f_x + f_x_h(h2, (i, 0)) + f_x_h(h2, (i, 1)))
-            df[i, i] /= 12 * np.abs(h[i]).mean()**2
+            df[i, i] /= 12 * np.abs(h[i]).mean(dtype=dtype)**2
             # off diagonal values
             for j in range(i):
                 df[i, j] = np.nan
