0f4cbe80bdad67fcec84cd59015e6e06b8473235
Joscha Reimer <jor@informatik.uni-kiel.de>
2020-01-30 16:06:30 +0100

    ENH: util.plot.auxiliary: y_axis_move_scientific_notation added

--- util/plot/auxiliary.py
+++ util/plot/auxiliary.py
@@ -21,7 +21,8 @@ def generic(file, plot_function, font_size=20, transparent=True, caption=None, u
             overwrite=True, make_read_only=True, dpi=800, backend=None,
             invert_x_axis=False, invert_y_axis=False,
             colorbar=False, colorbar_tick_number=None, colorbar_tick_step_size=None,
-            colorbar_tick_transform=None, colorbar_tick_power_limit_scientific=None):
+            colorbar_tick_transform=None, colorbar_tick_power_limit_scientific=None,
+            y_axis_move_scientific_notation_left=False):
 
     # check if file should be saved
     if check_file(file, overwrite=overwrite):
@@ -42,9 +43,15 @@ def generic(file, plot_function, font_size=20, transparent=True, caption=None, u
         axes = fig.gca()
 
         # power limits
-        set_tick_power_limit_scientific(tick_power_limit_scientific, axis='both')
+        if tick_power_limit_scientific is not None:
+            assert tick_power_limit_scientific_x is None
+            assert tick_power_limit_scientific_y is None
+            tick_power_limit_scientific_x = tick_power_limit_scientific
+            tick_power_limit_scientific_y = tick_power_limit_scientific
         set_tick_power_limit_scientific(tick_power_limit_scientific_x, axis='x')
         set_tick_power_limit_scientific(tick_power_limit_scientific_y, axis='y')
+        if tick_power_limit_scientific_y is not None and y_axis_move_scientific_notation_left:
+            y_axis_move_scientific_notation(limit_scientific_notation=tick_power_limit_scientific_y)
         set_tick_power_limit_fix(tick_power_limit_fix, axis='both')
         set_tick_power_limit_fix(tick_power_limit_fix_x, axis='x')
         set_tick_power_limit_fix(tick_power_limit_fix_y, axis='y')
@@ -375,3 +382,68 @@ def tick_transform_function_exponent_notation(data, v_max=None, decimals=None):
 
     # return
     return tick_transform
+
+
+def y_axis_move_scientific_notation(limit_scientific_notation, offs=0, dig=0, side='left', omit_last=False):
+    """Move scientific notation exponent from top to the side.
+
+    Additionally, one can set the number of digits after the comma
+    for the y-ticks, hence if it should state 1, 1.0, 1.00 and so forth.
+
+    Parameters
+    ----------
+    offs : float, optional; <0>
+        Horizontal movement additional to default.
+    dig : int, optional; <0>
+        Number of decimals after the comma.
+    side : string, optional; {<'left'>, 'right'}
+        To choose the side of the y-axis notation.
+    omit_last : bool, optional; <False>
+        If True, the top y-axis-label is omitted.
+
+    Returns
+    -------
+    locs : list
+        List of y-tick locations.
+
+    Note
+    ----
+    This is kind of a non-satisfying hack, which should be handled more
+    properly. But it works. Functions to look at for a better implementation:
+    ax.ticklabel_format
+    ax.yaxis.major.formatter.set_offset_string
+    """
+
+    # Get the ticks
+    locs, _ = plt.yticks()
+
+    # Put the last entry into a string, ensuring it is in scientific notation
+    # E.g: 123456789 => '1.235e+08'
+    llocs = '%.3e' % locs[-1]
+
+    # Get the magnitude, hence the number after the 'e'
+    # E.g: '1.235e+08' => 8
+    yoff = int(str(llocs).split('e')[1])
+
+    if yoff >= limit_scientific_notation:
+        # If omit_last, remove last entry
+        if omit_last:
+            slocs = locs[:-1]
+        else:
+            slocs = locs
+
+        # Set ticks to the requested precision
+        form = r'$%.' + str(dig) + 'f$'
+        plt.yticks(locs, list(map(lambda x: form % x, slocs / (10**yoff))))
+
+        # Define offset depending on the side
+        if side == 'left':
+            offs = -.18 - offs  # Default left: -0.18
+        elif side == 'right':
+            offs = 1 + offs     # Default right: 1.0
+
+        # Plot the exponent
+        plt.text(offs, .98, r'$\times10^{%i}$' % yoff, transform=plt.gca().transAxes, verticalalignment='top')
+
+    # Return the locs
+    return locs
